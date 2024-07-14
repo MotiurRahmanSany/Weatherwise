cc1bc192f6d112bdf6c92b1895bbd7dc43e283f5
MotiurRahmanSany <mdmotiurrahmansany@gmail.com>
2024-07-14 17:06:53 +0600

    testing git(nothing changed except blank space)

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+
       title: 'WeatherWise',
       home: HomeScreen(),
     );
   }
 }
+  
