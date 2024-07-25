f318499b327de514203906422410536e34547717
MotiurRahmanSany <mdmotiurrahmansany@gmail.com>
2024-07-25 21:38:18 +0600

    modified url structure

--- lib/utils/api_url.dart
+++ lib/utils/api_url.dart
@@ -2,9 +2,9 @@ import 'package:weatherwise/utils/api_key.dart';
 
 String getUrl(lat, lon) {
   const primaryUrl = 'https://api.openweathermap.org';
-  final path =
-      '/data/3.0/onecall?lat=$lat&lon=$lon&appid=$apiKey&units=metric&exclude=minutely';
-  final finalUrl = primaryUrl + path;
+  const path = '/data/3.0/onecall';
+  final queryString = '?lat=$lat&lon=$lon&appid=$apiKey&units=metric&exclude=minutely';
+  final finalUrl = primaryUrl + path + queryString;
 
   
   return finalUrl;
